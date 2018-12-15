6e2ad2a2eba18c50ae11588fc03087c8b27da60b
Packiaseelan <packiaseelan14@gmail.com>
2018-12-15 18:29:21 +0530

    struct embedded into another struct.

--- structs/main.go
+++ structs/main.go
@@ -5,17 +5,41 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	mobile  string
+	address addressInfo
+}
+
+type addressInfo struct {
+	doorNo     string
+	flatName   string
+	streetName string
+	city       string
+	zip        int
 }
 
 func main() {
-	//name := person{"go", "lang"}
-	//fmt.Println(name)
 
-	var name person
+	prsn := person{
+		firstName: "Packiaseelan",
+		lastName:  "Socretes",
+		contact: contactInfo{
+			email:  "packiaseelan14@gmail.com",
+			mobile: "1234567890",
+			address: addressInfo{
+				doorNo:     "C4",
+				flatName:   "Vinayaga flat",
+				streetName: "Pandian Street",
+				city:       "Chennai",
+				zip:        123456,
+			},
+		},
+	}
 
-	name.firstName = "go"
-	name.lastName = "lang"
-
-	fmt.Println(name)
-	fmt.Printf("%+v", name)
+	fmt.Println(prsn)
+	fmt.Printf("%+v", prsn)
 }

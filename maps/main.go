00a42109fbe2a9313240726d98037df28270faf9
Packiaseelan <packiaseelan14@gmail.com>
2018-12-16 17:19:04 +0530

    alternate ways to declare map and delete an item in map.

--- maps/main.go
+++ maps/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-		"blue":  "#0000ff",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// 	"blue":  "#0000ff",
+	// }
+
+	//multiple ways to create a map
+	//var colors map[string]string
+	colors := make(map[string]string)
+	colors["White"] = "#ffffff"
+	colors["Black"] = "#000000"
+
+	delete(colors, "White")
 
 	fmt.Println(colors)
 }
